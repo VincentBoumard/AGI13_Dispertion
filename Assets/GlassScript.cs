4a09ed258bac023dee17a4cd755150ec244cdc5b
vginman <vginman@kth.se>
2013-12-02 22:08:46 +0100

    Targets and some colour beams....

---
+++ Assets/GlassScript.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlassScript : MonoBehaviour
+{
+
+    public bool isLit = false;
+    public bool isLitOne = false;
+    private Vector3[] lightDirD = new Vector3[4];//(1.0f, 0.0f, 0.0f);
+    private Vector3 lightDir = new Vector3(1.0f, 0.0f, 0.0f);
+    private Vector3 lightPos = new Vector3(0.0f, 0.0f, 0.0f);
+    private Vector3 surfaceNormal = new Vector3(0.0f, 0.0f, 0.0f);
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            lightDirD[i] = new Vector3(1.0f, 0.0f, 0.0f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLit)
+        {
+            // used for debug purposes
+        }
+        else
+        {
+            Source src = this.gameObject.GetComponent<Source>();
+            if (src.isCreatedD)
+                src.DestroyDispersedLight();
+        }
+
+        if (isLitOne)
+        {
+            // used for debug purposes
+        }
+        else
+        {
+            Source src = this.gameObject.GetComponent<Source>();
+            if (src.isCreated)
+                src.DestroyLight();
+        }
+
+        // test function to rotate the mirrors in-game
+        if (Input.GetKeyDown("b"))
+            transform.Rotate(0.0f, 5.0f, 0.0f);
+    }
+
+    public bool getIsLit()
+    {
+        return isLit;
+    }
+
+    public bool getIsLitOne()
+    {
+        return isLitOne;
+    }
+
+    public Vector3[] getLightDir()
+    {
+        return lightDirD;
+    }
+
+    public Vector3 getLightDirOne()
+    {
+        return lightDir;
+    }
+
+    public Vector3 getLightPos()
+    {
+        return lightPos;
+    }
+
+    void lightsOff()
+    {
+        isLit = false;
+    }
+
+    public void Dispers(Vector3 coord, Vector3 normal, Vector3 lightRay)
+    {
+        isLit = true;
+        surfaceNormal = normal;
+        lightPos = coord;
+        Debug.Log("LightPos of dispersion" + lightPos);
+        for(int i = 0; i < 4; i++)
+        {
+            float angle = Vector3.Angle(lightRay, surfaceNormal);
+            // Vector3.Angle gives an absolute value, so we have to check if the reflected ray should be
+            // reflected to the right or to the left
+            angle = Vector3.Cross(lightRay, surfaceNormal).y > 0 ? angle : -angle;
+            Quaternion rot = Quaternion.AngleAxis(2*angle, this.transform.up);
+            lightDirD[i] = (rot * lightRay)+new Vector3(i, 0, 0);
+        }
+    }
+
+    public void DispersOne(Vector3 coord, Vector3 normal, Vector3 lightRay)
+    {
+        isLitOne = true;
+        surfaceNormal = normal;
+        lightPos = coord;
+        Debug.Log("LightPos of dispersion" + lightPos);
+        float angle = Vector3.Angle(lightRay, surfaceNormal);
+        // Vector3.Angle gives an absolute value, so we have to check if the reflected ray should be
+        // reflected to the right or to the left
+        angle = Vector3.Cross(lightRay, surfaceNormal).y > 0 ? angle : -angle;
+        Quaternion rot = Quaternion.AngleAxis(2 * angle, this.transform.up);
+        lightDir = (rot * lightRay);
+
+    }
+}
---
+++ Assets/LightBeamBlueScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightBeamBlueScript : MonoBehaviour
+{
+
+    public float distBlue;
+    public Vector3 forward;
+
+    // Use this for initialization
+    void Start()
+    {
+        Transform shadow = this.transform.FindChild("Shadow");
+
+        //Vector3 forward = this.transform.FindChild("Plane").transform.forward;
+        //shadow.position = Vector3.Normalize(transform.forward) * dist + new Vector3(0.0f, 2.8f, 0.0f);// - Vector3.Normalize(forward) * 0.5f
+        RaycastHit hit;
+        if (Physics.Raycast(this.transform.position, transform.forward, out hit))
+            distBlue = hit.distance;
+        shadow.gameObject.transform.position += transform.forward * (distBlue - 3.7f);//Vector3.Normalize(forward) * dist + new Vector3(0.0f, 2.8f, 0.0f);
+        Debug.Log(distBlue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
