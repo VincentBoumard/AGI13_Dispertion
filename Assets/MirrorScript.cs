27d1599e8698043a293af7b1cec23aba2dc9d3f4
vginman <vginman@kth.se>
2013-12-03 18:24:21 +0100

    Actual files...

--- Assets/MirrorScript.cs
+++ Assets/MirrorScript.cs
@@ -6,6 +6,7 @@ public class MirrorScript : MonoBehaviour {
 	public bool isLit = false;
 	private Vector3 lightDir = new Vector3(1.0f, 0.0f, 0.0f);
 	private Vector3 lightPos = new Vector3(0.0f, 0.0f, 0.0f);
+    private int lightType = 5;
 	private Vector3 surfaceNormal = new Vector3(0.0f, 0.0f, 0.0f);
 
 	// Use this for initialization
@@ -23,7 +24,7 @@ public class MirrorScript : MonoBehaviour {
 				src.DestroyLight();
 		}
 		// test function to rotate the mirrors in-game
-		if (Input.GetKeyDown ("a"))
+		if (Input.GetKeyDown ("v"))
 						transform.Rotate (0.0f, 5.0f, 0.0f);
 	}
 
@@ -38,15 +39,22 @@ public class MirrorScript : MonoBehaviour {
 	public Vector3 getLightPos(){
 		return lightPos;
 	}
+
+    public int getLightType()
+    {
+        return lightType;
+    }
 	
 	void lightsOff(){
 		isLit = false;
 	}
 
-	public void Reflect(Vector3 coord, Vector3 normal, Vector3 lightRay){
+	public void Reflect(Vector3 coord, Vector3 normal, Vector3 lightRay, int type){
 		isLit = true;
+        lightType = type;
 		surfaceNormal = normal;
 		lightPos = coord;
+        Debug.Log("LightPos of reflection" + lightPos);
 		float angle = Vector3.Angle (lightRay, surfaceNormal);
 		// Vector3.Angle gives an absolute value, so we have to check if the reflected ray should be
 		// reflected to the right or to the left
---
+++ Assets/SourceAuxG.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(GlassScript))]
+
+public class SourceAuxG : Source
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            base.lightDirD[i] = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+    }
+
+    // Update is called once per frame
+    new void Update()
+    {
+        GlassScript em = this.GetComponent<GlassScript>();
+        
+        if (em.getIsLit())
+        {
+            Vector3[] ldir = new Vector3[4];
+            ldir = em.getLightDir();
+
+            for(int i = 0; i < 4; i++)
+            {
+                base.lightDirD[i] = ldir[i];
+            }
+            base.lightPos = em.getLightPos();
+            base.lightType = 2;
+            base.CreateDispersedLight();
+            
+
+        }
+
+        else if (em.getIsLitOne())
+        {
+            base.lightDir = em.getLightDirOne();
+            base.lightPos = em.getLightPos();
+            base.lightType = em.getLightType();
+            base.CreateLight();
+        }
+        base.Update();
+    }
+}
