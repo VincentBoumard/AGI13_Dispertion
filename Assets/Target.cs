2bc32b7f3cc18fdbfd1e6aed89c36ee641aa3267
vginman <vginman@kth.se>
2013-12-04 17:03:50 +0100

    Yey

---
+++ Assets/Target.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour
+{
+
+    public bool isHit = false;
+    public int targetColour;
+
+    // Use this for initialization
+    void Start()
+    {
+        particleSystem.Pause();
+        //particleSystem.Clear();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isHit)
+        {
+            particleSystem.Play();
+            // used for debug purposes
+        }
+        else
+        {            
+            particleSystem.Pause();
+            particleSystem.Clear();
+        }
+        // test function to rotate the mirrors in-game
+        if (Input.GetKeyDown("d"))
+            transform.Translate(0.5f, 0.0f, 0.0f);
+        if (Input.GetKeyDown("a"))
+            transform.Translate(-0.5f, 0.0f, 0.0f);
+        if (Input.GetKeyDown("w"))
+            transform.Translate(0.0f, 0.0f, 0.5f);
+        if (Input.GetKeyDown("s"))
+            transform.Translate(0.0f, 0.0f, -0.5f);
+
+    }
+
+    public void Hit()
+    {
+        isHit = true;
+    }
+
+    public void setColour(int i)
+    {
+        targetColour = i;
+    }
+
+    public int getColour()
+    {
+        return targetColour;
+    }
+}
